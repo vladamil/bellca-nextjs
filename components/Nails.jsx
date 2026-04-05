be23a351f174e72335571d1afb07748964c54a76
Vladimir Milinic <vladimir.milinic@gmail.com>
2026-04-05 21:56:18 +0200

    Finish nails service section and install react-icons

--- components/Nails.jsx
+++ components/Nails.jsx
@@ -1,3 +1,5 @@
+import Image from 'next/image';
+import { FaStar } from 'react-icons/fa';
 import styles from './Nails.module.css';
 
 export default function Nails({ nails }) {
@@ -9,35 +11,36 @@ export default function Nails({ nails }) {
 
                <div className={styles.services}>
                   <h3>Noktići</h3>
+
                   <p>
-                     <i class="fa-solid fa-star"></i>
+                     <FaStar className={styles.icon} />
                      manikir
                   </p>
                   <p>
-                     <i class="fa-solid fa-star"></i>
+                     <FaStar className={styles.icon} />
                      gellak
                   </p>
                   <p>
-                     <i class="fa-solid fa-star"></i>
+                     <FaStar className={styles.icon} />
                      ojačavanje prirodnih noktiju (rubber bazom ili gelom
                      zavisno od tipa nokatne ploče)
                   </p>
                   <p>
-                     <i class="fa-solid fa-star"></i>
+                     <FaStar className={styles.icon} />
                      izlivanje (tehnikom dual formi, gornjih i donjih )
                   </p>
                   <p>
-                     <i class="fa-solid fa-star"></i>
+                     <FaStar className={styles.icon} />
                      nadogradnja Gel x tipsama (tipse nove generacije koje su
                      izrađene od gela, a apliciraju se na nokat rubber bazom
                      umesto lepkom)
                   </p>
                   <p>
-                     <i class="fa-solid fa-star"></i>
+                     <FaStar className={styles.icon} />
                      građeni frenč
                   </p>
                   <p>
-                     <i class="fa-solid fa-star"></i>
+                     <FaStar className={styles.icon} />
                      tehnike ukrašavanja : frenč, ombre, fade, cirkoni, nail
                      art, transfer folije, blossom, cat eye, aurora
                   </p>
@@ -47,52 +50,132 @@ export default function Nails({ nails }) {
 
                <div className={styles.bentoGrid}>
                   <div className={styles.item}>
-                     <img src={nails[0]} alt="Nails" />
+                     <Image
+                        src={nails[0]}
+                        fill
+                        sizes="(max-width: 768px) 30vw, (max-width: 1200px) 50vw, 33vw"
+                        alt="Nails"
+                     />
                   </div>
                   <div className={`${styles.item} ${styles.wide}`}>
-                     <img src={nails[1]} alt="Nails" />
+                     <Image
+                        src={nails[1]}
+                        fill
+                        sizes="(max-width: 768px) 30vw, (max-width: 1200px) 50vw, 33vw"
+                        alt="Nails"
+                     />
                   </div>
                   <div className={styles.item}>
-                     <img src={nails[2]} alt="Nails" />
+                     <Image
+                        src={nails[2]}
+                        fill
+                        sizes="(max-width: 768px) 30vw, (max-width: 1200px) 50vw, 33vw"
+                        alt="Nails"
+                     />
                   </div>
                   <div className={styles.item}>
-                     <img src={nails[3]} alt="Nails" />
+                     <Image
+                        src={nails[3]}
+                        fill
+                        sizes="(max-width: 768px) 30vw, (max-width: 1200px) 50vw, 33vw"
+                        alt="Nails"
+                     />
                   </div>
                   <div className={`${styles.item} ${styles.wide}`}>
-                     <img src={nails[4]} alt="Nails" />
+                     <Image
+                        src={nails[4]}
+                        fill
+                        sizes="(max-width: 768px) 30vw, (max-width: 1200px) 50vw, 33vw"
+                        alt="Nails"
+                     />
                   </div>
                   <div className={`${styles.item} ${styles.full}`}>
-                     <img src={nails[5]} alt="Nails" />
+                     <Image
+                        src={nails[5]}
+                        fill
+                        sizes="(max-width: 768px) 30vw, (max-width: 1200px) 50vw, 33vw"
+                        alt="Nails"
+                     />
                   </div>
                   <div className={styles.item}>
-                     <img src={nails[6]} alt="Nails" />
+                     <Image
+                        src={nails[6]}
+                        fill
+                        sizes="(max-width: 768px) 30vw, (max-width: 1200px) 50vw, 33vw"
+                        alt="Nails"
+                     />
                   </div>
                   <div className={styles.item}>
-                     <img src={nails[7]} alt="Nails" />
+                     <Image
+                        src={nails[7]}
+                        fill
+                        sizes="(max-width: 768px) 30vw, (max-width: 1200px) 50vw, 33vw"
+                        alt="Nails"
+                     />
                   </div>
                   <div className={styles.item}>
-                     <img src={nails[8]} alt="Nails" />
+                     <Image
+                        src={nails[8]}
+                        fill
+                        sizes="(max-width: 768px) 30vw, (max-width: 1200px) 50vw, 33vw"
+                        alt="Nails"
+                     />
                   </div>
                   <div className={`${styles.item} ${styles.tall}`}>
-                     <img src={nails[9]} alt="Nails" />
+                     <Image
+                        src={nails[9]}
+                        fill
+                        sizes="(max-width: 768px) 30vw, (max-width: 1200px) 50vw, 33vw"
+                        alt="Nails"
+                     />
                   </div>
                   <div className={styles.item}>
-                     <img src={nails[10]} alt="Nails" />
+                     <Image
+                        src={nails[10]}
+                        fill
+                        sizes="(max-width: 768px) 30vw, (max-width: 1200px) 50vw, 33vw"
+                        alt="Nails"
+                     />
                   </div>
                   <div className={styles.item}>
-                     <img src={nails[11]} alt="Nails" />
+                     <Image
+                        src={nails[11]}
+                        fill
+                        sizes="(max-width: 768px) 30vw, (max-width: 1200px) 50vw, 33vw"
+                        alt="Nails"
+                     />
                   </div>
                   <div className={`${styles.item} ${styles.tall}`}>
-                     <img src={nails[12]} alt="Nails" />
+                     <Image
+                        src={nails[12]}
+                        fill
+                        sizes="(max-width: 768px) 30vw, (max-width: 1200px) 50vw, 33vw"
+                        alt="Nails"
+                     />
                   </div>
                   <div className={styles.item}>
-                     <img src={nails[13]} alt="Nails" />
+                     <Image
+                        src={nails[13]}
+                        fill
+                        sizes="(max-width: 768px) 30vw, (max-width: 1200px) 50vw, 33vw"
+                        alt="Nails"
+                     />
                   </div>
                   <div className={`${styles.item} ${styles.wide}`}>
-                     <img src={nails[14]} alt="Nails" />
+                     <Image
+                        src={nails[14]}
+                        fill
+                        sizes="(max-width: 768px) 30vw, (max-width: 1200px) 50vw, 33vw"
+                        alt="Nails"
+                     />
                   </div>
                   <div className={styles.item}>
-                     <img src={nails[15]} alt="Nails" />
+                     <Image
+                        src={nails[15]}
+                        fill
+                        sizes="(max-width: 768px) 30vw, (max-width: 1200px) 50vw, 33vw"
+                        alt="Nails"
+                     />
                   </div>
                </div>
             </div>
